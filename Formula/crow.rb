781de4dc775762eafe6684a7c34408caf400a994
Theo Salzmann <theo.salzmann@sandstorm.de>
2021-11-13 09:06:03 +0100

    Up version

--- Formula/crow.rb
+++ Formula/crow.rb
@@ -4,7 +4,7 @@
 class Crow < Formula
   desc "Save cli commands and fuzzy find them later"
   homepage "https://github.com/sandstorm/crow"
-  url "https://github.com/sandstorm/crow/releases/download/v0.1.0-beta-5/crow.tar.gz"
+  url "https://github.com/sandstorm/crow/releases/download/v0.1.0-beta-6/crow.tar.gz"
   sha256 "42dfd37c7a9ea8e5ba51ea0b82103e293688e90f66f69a6c5fbd33c9cc59f104"
   license "MIT"
 
