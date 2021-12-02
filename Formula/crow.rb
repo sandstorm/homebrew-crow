97eda4b95fceeb95f176f365cdf744adcfc462ff
Theo Salzmann <dev@on3iro.de>
2021-12-02 07:57:11 +0100

    crow 0.5.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/crow.rb
+++ Formula/crow.rb
@@ -4,8 +4,8 @@
 class Crow < Formula
   desc "Save cli commands and fuzzy find them later"
   homepage "https://github.com/sandstorm/crow"
-  url "https://github.com/sandstorm/crow/releases/download/v0.5.1/crow.tar.gz"
-  sha256 "45a6d91568b24ed3e561377978d98f4c747d0f1d74eebf9b986ca3b8ef664a83"
+  url "https://github.com/sandstorm/crow/releases/download/v0.5.2/crow.tar.gz"
+  sha256 "daa89d6bd14bee852f374f34f128ffed0b41451e942238381bcf129a17df8076"
   license "MIT"
 
   def install
